969ca292346c2302e123627d8999309b3ca22aed
Markus Frindt <markus.frindt@sovanta.com>
2018-02-23 20:20:56 +0100

    merge, add /build to gitignore

--- contracts/TimeDeliveryManagement.sol
+++ contracts/TimeDeliveryManagement.sol
@@ -1,69 +1,111 @@
+
+//import "https://github.com/Arachnid/solidity-stringutils/blob/master/strings.sol";
+
+//https://github.com/willitscale/solidity-util#concatstring--string kopiert
+import "./Strings.sol";
+
 pragma solidity ^0.4.0;
+
 contract TimeDeliveryManagement {
 
-   string public debug ;
+    using Strings for string;
+    string public debug ;
 
-   struct DeliverySlot{
-       uint256 id;
-       bool    isTradeable;
-       string  timeFrom;
-       string  timeTo;
-       uint    price;
-       string  gate;
-       string  warehouseName;
-       string  deliveryType;
-   }
+    struct DeliverySlot{
+        uint id;
+        bool    isTradeable;
+        uint  timeFrom;
+        uint  timeTo;
+        uint    price;
+        string  gate;
+        string  warehousename;
+        string  logisticType;
+    }
 
-   struct Supplier {
-       address userId;
-       DeliverySlot[] deliverySlots;
-   }
+    struct Supplier {
+        address userId;
+        mapping (uint => DeliverySlot) deliverySlots;
+        //DeliverySlot[] deliverySlots;
+    }
 
-   uint256 public maxid;
+    uint public maxid;
 
-   address public warehouse;
-   mapping(address => Supplier) suppliers;
+    address public warehouse;
+    mapping(address => Supplier) suppliers;
 
 
-   function TimeDeliveryManagement() public {
-           warehouse = msg.sender;
-           debug = "wir gewinnen";
-           maxid = 0;
-       }
+    function TimeDeliveryManagement() public {
+            warehouse = msg.sender;
+            debug = "wir gewinnen";
+            maxid =0;
+        }
 
-   function createDeliverySlot()  public {
-       if (msg.sender != warehouse) return;
-            suppliers[msg.sender].deliverySlots.push(DeliverySlot({
-               id: maxid++, isTradeable: true, timeFrom: "gestern", timeTo: "heute", price:5, gate:"123", warehouseName: "Warenhaus1", deliveryType: "Eis"
-           }));
-           debug = "hat geklappt";
-   }
+    function createDeliverySlot()  public returns (uint currentID
+      ) {
+        if (msg.sender != warehouse) return;
+        currentID = maxid++;
+        suppliers[msg.sender].deliverySlots[currentID] = DeliverySlot(currentID,true,1245,1234,5,"123","Warenhaus1", "Food" );
+        debug = "hat geklappt";
+        return currentID;
+    }
 
-   function update(uint256 id, bool isTradeable, string from, string to, uint price, string gate, string warehouseName, string deliveryType)  public {
-       if (msg.sender != warehouse) return;
-            suppliers[msg.sender].deliverySlots.push(DeliverySlot({
-              id: id, isTradeable: isTradeable, timeFrom: from, timeTo: to, price: price, gate: gate, warehouseName: warehouseName, deliveryType: deliveryType
-           }));
-           debug = "hat geklappt";
-   }
+    function getFirstDeliverySlots(uint indexofDS) public returns (string success)
+    {
+        //prüfe Vergangeheit
+        success = suppliers[warehouse].deliverySlots[indexofDS].warehousename;
+        return success;
+    }
 
-   function getFirstDeliverySlots(uint256 indexofDS) public returns (string success)
-   {
-       success = suppliers[warehouse].deliverySlots[indexofDS].timeFrom;
-   }
+    function getAllDeliverySlots(string searchstring) public returns (string success)
+    {
 
-   function getAllDeliverySlots(string searchstring) public returns (string success)
-   {
-       string storage s1 = suppliers[warehouse].deliverySlots[0].timeFrom;
-       string storage s2 = suppliers[warehouse].deliverySlots[0].timeTo;
-       success = s1;
-   }
+        success = "{[";
 
-   function bookDeliverySlot(uint256 idOfDS) public returns (string success)
-   {
-     DeliverySlot storage deSlot = suppliers[warehouse].deliverySlots[0];
-     deSlot.isTradeable = false;
-     suppliers[msg.sender].deliverySlots.push(deSlot);
-   }
+        for (uint i = 1; i < maxid; i++) {
+          //check if Old
+          if(myDelSlot.timeTo >   block.timestamp) continue;
+          //check if available
+
+          success.concat("{");
+          DeliverySlot storage myDelSlot = suppliers[warehouse].deliverySlots[i];
+          success.concat(myDelSlot.warehousename).concat(" , ");
+          //success.concat(myDelSlot.timeFrom).concat(" , ");
+          //success.concat(myDelSlot.timeTo).concat(" , ");
+          success.concat(myDelSlot.gate).concat(" , ");
+          success.concat(myDelSlot.logisticType).concat(" , ");
+          success.concat("}");
+        }
+        success.concat("]}");
+
+        return success;
+    }
+
+    function purchaseDeliverySlot(uint idOfDS) public returns (bool success)
+    {
+      DeliverySlot storage deSlot = suppliers[warehouse].deliverySlots[idOfDS];
+      //check if Old
+      if(myDelSlot.timeTo >   block.timestamp) return false;
+
+      deSlot.isTradeable = false;
+      suppliers[msg.sender].deliverySlots[idOfDS] = deSlot;
+      return true;
+    }
+
+    function offerDeliverySlot(uint idOfDS) public returns (bool success)
+    {
+      DeliverySlot storage deSlot =suppliers[msg.sender].deliverySlots[idOfDS] = deSlot;
+      //check if Old
+      if(myDelSlot.timeTo >   block.timestamp) return false;
+
+      deSlot.isTradeable = true;
+      return true;
+    }
+
+    function setBuyOrderDeliverySlot() public returns (bool success)
+    {
+
+      //Foreach available Slot
+      return true;
+    }
 
 }
